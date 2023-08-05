647a291695e92a033f622e1feb8b9ecb371b9ced
Aakash Apoorv <aakashapoorv@users.noreply.github.com>
2023-08-05 13:34:15 +0200

    feat: add aakash.sh

---
+++ aakash.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+speed=1.0
+speedFast=0.5
+
+sleep $speedFast
+echo " ________  ________  ___  __    ________  ________  ___  ___     "
+sleep $speedFast
+echo "|\   __  \|\   __  \|\  \|\  \ |\   __  \|\   ____\|\  \|\  \    "
+sleep $speedFast
+echo "\\ \\  \\|\\  \\ \\  \\|\\  \\ \\  \\/  /|\\ \\  \\|\\  \\ \\  \\___|\\ \\  \\\\\\  \\"
+sleep $speedFast
+echo " \ \   __  \ \   __  \ \   ___  \ \   __  \ \_____  \ \   __  \  "
+sleep $speedFast
+echo "  \\ \\  \\ \\  \\ \\  \\ \\  \\ \\  \\\\ \\  \\ \\  \\ \\  \\|____|\\  \\ \\  \\ \\  \\ "
+sleep $speedFast
+echo "   \ \__\ \__\ \__\ \__\ \__\\\ \__\ \__\ \__\____\\_\  \ \__\ \__\\"
+sleep $speedFast
+echo "    \|__|\|__|\|__|\|__|\|__| \|__|\|__|\|__|\_________\|__|\|__|"
+sleep $speedFast
+echo "                                            \|_________|         "
+sleep $speedFast
+echo "                                                                 "
+sleep $speedFast
+echo "                                                                 "
+sleep $speedFast
+
+
+sleep $speed
+echo ""
+sleep $speed
+echo "==> Contact https://www.linkedin.com/in/serverless/"                              
+sleep $speed
+echo ""
+
+sleep $speed
+echo "==> Installing Aakash Apoorv: Full Stack Engineer | Head of Development | Research Engineer"
+sleep $speed
+echo "==> Pouring skills--10-years-experience.bottle.tar.gz"
+sleep $speed
+echo "🍺  Proficient in: Javascript, Typescript, Python"
+sleep $speed
+echo "🍺  Working knowledge of: Java, C#, Golang, C++"
+sleep $speed
+echo "🍺  Frameworks: React.js, Express.js, Next.js, Spring Boot, ASP.NET"
+sleep $speed
+echo "🍺  Databases: SQL, Postgres, DynamoDB, MongoDB, Pinecone"
+sleep $speed
+echo "🍺  Other Technologies: Serverless, Microservices, Micro-frontend, Docker, Kubernetes, Deep learning, LLM, AWS, WebGL, Unity"
+sleep $speed
+echo ""
+sleep $speed
+echo "==> Installing Work Experience: Vince AS | Head of Development"
+sleep $speed
+echo "==> Pouring vince-as--head-of-development.bottle.tar.gz"
+sleep $speed
+echo "🍺  NOV 2021 – July 2023 | Oslo, Norway"
+sleep $speed
+echo "🍺  Responsibilities: Hired and mentored the dev team, Supervised engineering of Enterprise SaaS product, Built and trained LLM models on Enterprise data, Used JS/TS, Python, Microservices, Micro-frontend"
+sleep $speed
+echo ""
+sleep $speed
+echo "==> Installing Work Experience: Omega 365 Solutions AS | Senior System Engineer"
+sleep $speed
+echo "==> Pouring omega-365--senior-system-engineer.bottle.tar.gz"
+sleep $speed
+echo "🍺  JAN 2020 – OCT 2021 | Oslo, Norway"
+sleep $speed
+echo "🍺  Built mega digital twin for the oil platforms, Used JS/TS, C#, WebGL, .Net Core, Unity, React"
+sleep $speed
+echo ""
+sleep $speed
+echo "==> Installing Work Experience: Delivery Hero | Engineering Manager"
+sleep $speed
+echo "==> Pouring delivery-hero--engineering-manager.bottle.tar.gz"
+sleep $speed
+echo "🍺  MAR 2019 – DEC 2019 | Singapore"
+sleep $speed
+echo "🍺  Hired and mentored the dev team, Architected high throughput microservices and micro-frontend, 1st Engineer in APAC helped to scale to 70 people, Used JS/TS, Golang, React, Serverless"
+sleep $speed
+echo ""
+sleep $speed
+echo "==> Installing Education: Master in Information Technology"
+sleep $speed
+echo "==> Pouring master-it--james-cook-university.bottle.tar.gz"
+sleep $speed
+echo "🍺  Computing & Networking | James Cook University, Australia, 2012"
+sleep $speed
+echo ""
+sleep $speed
+echo "==> Installing Education: Bachelor of Science | Computer Science"
+sleep $speed
+echo "==> Pouring bachelor-cs--mumbai-university.bottle.tar.gz"
+sleep $speed
+echo "🍺  Mumbai University, India, 2010"
+sleep $speed
+echo ""
+sleep $speed
+echo "==> Installing Open Source Contribution: Redux"
+sleep $speed
+echo "==> Pouring redux--optimized-object-check.bottle.tar.gz"
+sleep $speed
+echo "🍺  Recommended an optimized plain object check function, closed due to inter-realm isolation"
+sleep $speed
+echo ""
+sleep $speed
+echo "==> Installing Open Source Contribution: Mono"
+sleep $speed
+echo "==> Pouring mono--open-source-dotnet.bottle.tar.gz"
+sleep $speed
+echo "🍺  Contributed to the mono project, it’s an open-source implementation of .NET Framework"
+sleep $speed
+echo ""
+sleep $speed
+echo "==> Installing Open Source Contribution: FFWD | Xbox 360"
+sleep $speed
+echo "==> Pouring ffwd--max-magic-marker.bottle.tar.gz"
+sleep $speed
+echo "🍺  Framework was used to make Max & the Magic Marker, published by Electronic Arts and Microsoft Game Studios"
+sleep $speed
+echo ""
+sleep $speed
+echo "==> Running \`cleanup Aakash Apoorv's CV\`..."
+sleep $speed
+echo "Github: https://github.com/aakashapoorv"
+sleep $speed
+echo "Linkedin: https://www.linkedin.com/in/serverless"
